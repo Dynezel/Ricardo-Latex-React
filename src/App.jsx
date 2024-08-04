b13083ea2ce4eba290ef996b40d4e4dc3109cffd
Dynezel <dylan50xd@gmail.com>
2024-08-03 22:27:26 -0300

    cambios UI

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Routes>
             <Route path="/" element={ <LatexContentList/> } />
             <Route path="/latex/:id" element={ <LatexContentDetail/> } />
-            <Route path="/create" element={ <CreateLatexContent/> } />
+            <Route path="/create/3d9b4c8e-2764-4d1f-84df-12fa9a123d49-93b1f0d9c7a8a9cba5e9d8c3a7b2f4e6" element={ <CreateLatexContent/> } />
             <Route path="/update/:id" element={ <UpdateLatexContent/> } />
             <Route path="/delete/:id" element={ <DeleteLatexContent/> } />
             <Route path="/usuario/registro" element= { <RegistrarUsuario/> } />
