d32fd2a62cc321e4a4833d676270b30ea5c44069
Dynezel <dylan50xd@gmail.com>
2024-12-12 21:24:03 -0300

    Cambio texto para ir a inicio

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
+    <div className="centro-pagina">
     <Link to="/" className="inicio"> <strong> Ricardo </strong></Link>
+    </div>
   )
 }
